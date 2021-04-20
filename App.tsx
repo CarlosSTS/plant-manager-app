98f4086d8c61984f8d0064815091924e7b1fbbd6
Francisco Carlos Ferreira <franciscocarlosferreira@Franciscos-MacBook-Pro.local>
2021-04-20 11:13:16 -0300

    Aula #1

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Welcome from "./src/pages/Welcome";
+
+const App: React.FC = () => {
+  return <Welcome />;
+}
+
+export default App;
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Text, View, Image, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+
+import colors from '../../styles/colors';
+import wateringImg from "../assets/watering.png"
+
+import Button from "../components/Button"
+
+const Welcome: React.FC = () => {
+  return (
+
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Gerencie {'\n'}
+       suas plantas {'\n'}
+        de forma fácil</Text>
+
+      <Image source={wateringImg} />
+
+      <Text style={styles.subTitle}>
+        não esqueça mais de regar suas plantas.
+        Nós cuidamos de lembrar você sempre que precisar.
+      </Text>
+<Button title=">"/>
+    </SafeAreaView>
+
+  )
+}
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+  },
+  subTitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading
+  },
+ 
+});
